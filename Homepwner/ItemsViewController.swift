e1fe2167c181f46292c5ff3d45a76f4b02641cf5
Muhammad Ashary <muh.ariashary@gmail.com>
2019-08-14 23:17:03 +0700

    Initial commit

---
+++ Homepwner/ItemsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Muhammad Ashary on 14/08/19.
+//  Copyright © 2019 M. Ashary. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+    
+    
+}
