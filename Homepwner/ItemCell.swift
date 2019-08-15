1d501fcac1ee429f080062b403f425e783fa97f5
Muhammad Ashary <muh.ariashary@gmail.com>
2019-08-15 18:35:07 +0700

    Add custom tableViewCell

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Muhammad Ashary on 15/08/19.
+//  Copyright © 2019 M. Ashary. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+}
--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -44,6 +44,7 @@ class ItemsViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -52,15 +53,17 @@ class ItemsViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Get a new or recycled cell
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
         
         // Set the text on the cell with the description of the item
         // that is at the nth index of items, where n = row this cell
         // will appear in on the tableView
         let item = itemStore.allItems[indexPath.row]
         
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        // Configure the cell with the Item
+        cell.nameLabel.text = item.name
+        cell.serialNumberLabel.text = item.serialNumber
+        cell.valueLabel.text = "$ \(item.valueInDollars)"
         
         return cell
     }
