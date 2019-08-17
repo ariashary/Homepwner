52c4ae7a67cf1c35beb41ca50382ba0d54d0dc4a
Muhammad Ashary <muh.ariashary@gmail.com>
2019-08-17 14:45:42 +0700

    Implement saving and loading methods

--- Homepwner/ItemStore.swift
+++ Homepwner/ItemStore.swift
@@ -12,6 +12,12 @@ class ItemStore {
     
     var allItems = [Item]()
     
+    let itemArchiveURL: URL = {
+        let documentDirectories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentDirectories.first!
+        return documentDirectory.appendingPathComponent("item.archive")
+    }()
+    
     @discardableResult func createItem() -> Item {
         let newItem = Item(random: true)
         
@@ -40,4 +46,26 @@ class ItemStore {
         // Insert item in array at new location
         allItems.insert(movedItem, at: toIndex)
     }
+    
+    func saveChanges() -> Bool {
+        print("Saving items to: \(itemArchiveURL.path)")
+        
+        return NSKeyedArchiver.archiveRootObject(allItems, toFile: itemArchiveURL.path)
+        
+        /*
+        do {
+            let data = try NSKeyedArchiver.archivedData(withRootObject: allItems, requiringSecureCoding: false)
+            return try data.write(to: itemArchiveURL)
+        } catch {
+            print(error)
+        }
+        */
+    }
+    
+    init() {
+        if let archivedItems = NSKeyedUnarchiver.unarchiveObject(withFile: itemArchiveURL.path) as? [Item] {
+            allItems = archivedItems
+        }
+    }
+    
 }
